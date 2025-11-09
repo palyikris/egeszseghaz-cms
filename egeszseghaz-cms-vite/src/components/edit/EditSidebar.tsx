2d34349366789c8b20394cbf69d1b2c633a65718
Kribbany <palyi.kristof@gmail.com>
2025-11-09 18:58:06 +0100

    refactor: improve EditSidebar logic by simplifying conditionals and enhancing readability

--- egeszseghaz-cms-vite/src/components/edit/EditSidebar.tsx
+++ egeszseghaz-cms-vite/src/components/edit/EditSidebar.tsx
@@ -5,7 +5,7 @@ import { HeroEditor } from "./editors/HomeEditor";
 export function EditSidebar() {
   const { isEditMode, selectedId } = useEditMode();
 
-  if (!isEditMode || !selectedId) return null;
+  if (!isEditMode) return null;
 
   const componentMap: Record<string, React.ReactNode> = {
     // Here you can map selectedId to specific editor components
@@ -14,12 +14,14 @@ export function EditSidebar() {
     hero: <HeroEditor />,
   };
 
-  const editorComponent = componentMap[selectedId];
+  const editorComponent = selectedId ? componentMap[selectedId] : null;
 
   return (
-    <div className="fixed top-0 right-0 h-full w-1/3 border-l border-border shadow-xl z-[10000] flex flex-col p-4 max-h-screen overflow-y-scroll">
+    <div
+      className={`fixed top-0 right-0 h-full w-1/3 border-l border-border shadow-xl z-[10000] flex flex-col p-4 max-h-screen overflow-y-scroll transform ${selectedId ? "opacity-100" : "opacity-0 pointer-events-none translate-x-2"} transition-all duration-200`}
+    >
       <h2 className="text-lg font-semibold text-primary-dark mb-2 border-b border-primary-dark">
-        Edit Component: {selectedId.toUpperCase()}
+        Edit Component: {selectedId?.toUpperCase()}
       </h2>
       <div className="fixed -z-1 backdrop-blur-md bg-primary-light/80 w-1/3 right-0 h-full top-0" />
       {editorComponent}
